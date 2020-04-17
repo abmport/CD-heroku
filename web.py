986890a9d222e5687f99cfba233452f837138a88
root <abnerzao@gmail.com>
2020-04-17 19:12:35 -0300

    Teste 4.0

--- web.py
+++ web.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='template')
 
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 @app.route("/index.html")
 def index():
