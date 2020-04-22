79e5b56b31c923e671961d1ed1e5bc80044082f1
Abner Porto <56705676+abmport@users.noreply.github.com>
2020-04-22 11:11:04 -0300

    teste4

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Olá mundo!v2"
+    return "Olá mundo! Teste 4"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
