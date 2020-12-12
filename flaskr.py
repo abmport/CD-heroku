a0d528cda8b1407e94d370212258e270d36fbbdd
abmport <abner.porto@aluno.faculdadeimpacta.com.br>
2020-12-12 00:45:34 -0300

    lista de compras

---
+++ flaskr.py
@@ -0,0 +1,46 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+    abort, render_template, flash
+from contextlib import closing
+
+DATABASE = './tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('CONFIG_FLASKR', silent=True)
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def criar_bd():
+    with closing(conectar_bd()) as bd:
+        with app.open_resource('banco.sql') as sql:
+            bd.cursor().executescript(sql.read().decode('utf-8'))
+        bd.commit()
+
+@app.before_request
+def pre_requisecao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def encerrar_requisicao(exception):
+    g.bd.close()
+
+@app.route('/')
+def principal():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+if __name__ == "__main__":
+    app.run()
