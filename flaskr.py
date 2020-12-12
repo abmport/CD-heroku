df78046935cc964832a67470cdcecadb4932b14e
abmport <abner.porto@aluno.faculdadeimpacta.com.br>
2020-12-12 00:48:16 -0300

    flask

--- flaskr.py
+++ flaskr.py
@@ -43,4 +43,7 @@ def cadastro():
     return render_template('cadastro.html')
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
+   
